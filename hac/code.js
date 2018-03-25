d0abeee1d5a4ecf8c84047ef7e30b693835b1b6f
Tim Remmerswaal <remmer.mit@gmail.com>
2018-03-25 15:24:04 +0200

    Added /hac page

---
+++ hac/code.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+    
+    setInterval(function() {
+      navbarswitchdirection();
+}, 20);
+
+function navbarswitchdirection() {
+
+var $rightnavbarcontent = 0;
+var $navbarcontainer = 0;
+$rightnavbarcontent=$rightnavbarcontent+$(".navhome").outerWidth();
+$rightnavbarcontent=$rightnavbarcontent+$(".navcrew").outerWidth();
+$rightnavbarcontent=$rightnavbarcontent+$(".navcontact").outerWidth();
+$rightnavbarcontent=$rightnavbarcontent+$(".navoverons").outerWidth();
+
+$navbarcontainer=$navbarcontainer+$(".navbar-flexbox").outerWidth();
+
+if($rightnavbarcontent>$navbarcontainer) {
+	$(".flex-right").css("flex-direction", "column");
+} else {
+	$(".flex-right").css("flex-direction", "row");
+}
+
+console.log($rightnavbarcontent);
+console.log($navbarcontainer);
+
+}
+    
+});
